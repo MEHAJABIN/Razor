741d94e5c6ff5c2e2b8834b92d74d051ff4c198e
MEHAJABIN <mizna.arakkal@gmail.com>
2019-04-04 13:39:50 +0530

    Index.cshtml.cs code updated

--- Razor/Pages/Index.cshtml.cs
+++ Razor/Pages/Index.cshtml.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Razor.Models;
 
 namespace Razor.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
-              private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+        //public void OnGet()
+        
+              private readonly Razor.Models.RazorContext _context;
 
-        public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+        public IndexModel(Razor.Models.RazorContext context)
         {
             _context = context;
         }
@@ -24,14 +27,22 @@ namespace Razor.Pages
         // Requires using Microsoft.AspNetCore.Mvc.Rendering;
         public SelectList Genres { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string MovieGenre { get; set; }*/
+        public string MovieGenre { get; set; }
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
 
+            Movie = await movies.ToListAsync();
         }
+
+
+
+
+    }
 }
-}
 
-
-
-/*
-{
-  
